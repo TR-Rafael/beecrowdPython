c4fc9655bde7e99e69f2685549cbb26a67ccbdfa
TR <silva_rafael@id.uff.br>
2024-05-14 12:42:53 -0300

    feat: finish question 1006

---
+++ 1006.py
@@ -0,0 +1,8 @@
+A = float(input())
+weight_A = 2
+B = float(input())
+weight_B = 3
+C = float(input())
+weight_C = 5
+
+print(f'MEDIA = {((A * weight_A + B * weight_B + C * weight_C)/(weight_A + weight_B + weight_C)):.1f}')
