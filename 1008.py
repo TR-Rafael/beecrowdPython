7fbfccd9c93849550f855d72be195022bd01ae15
TR <silva_rafael@id.uff.br>
2024-05-14 13:16:22 -0300

    feat: finish question 1008

---
+++ 1008.py
@@ -0,0 +1,6 @@
+number = int(input())
+worked_hours_amount = int(input())
+amount_the_employee_receives_per_worked_hour = float(input())
+
+print(f'NUMBER = {number}')
+print(f'SALARY = U$ {(worked_hours_amount * amount_the_employee_receives_per_worked_hour):.2f}')
