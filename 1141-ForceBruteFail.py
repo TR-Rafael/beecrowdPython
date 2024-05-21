23fee7104837853ad7c1dd8256584aef7b3f92d0
TR <silva_rafael@id.uff.br>
2024-05-21 16:35:03 -0300

    feat: advancement on issue 1141

--- 1141-ForceBruteFail.py
+++ 1141-ForceBruteFail.py
@@ -1,17 +1,17 @@
-# Forca Bruta que nao funcionou... Deu time limit exceeded. 6 segundos de runtime...
 import sys
 
+def check_farm_of_strings(list_of_string_sorted):
+    answer = 0
+    for i in range(len(list_of_string_sorted)):
+        aux = 0
+        current_string = list_of_string_sorted[i]
+        for string in list_of_string_sorted[i+1::]:
+            if current_string in string and len(current_string) <= len(string):
+                aux += 1
+                current_string = string
+        answer = max(aux, answer)
 
-def check_farm_of_strings(list_of_string_sorted, current_string):
-    count_aux_list = []
-    index_of_aux_list = 0
-    count_aux_list.append(current_string)
-    for current_string in list_of_string_sorted:
-        if count_aux_list[index_of_aux_list] in current_string:
-            index_of_aux_list += 1
-            count_aux_list.append(current_string)
-
-    return len(count_aux_list) - 1
+    return answer
 
 
 for line in sys.stdin:
@@ -23,7 +23,5 @@ for line in sys.stdin:
     list_of_string = []
     for i in range(int(N)):
         list_of_string.append(input())
-
     sorted_list = sorted(list_of_string, key=len)
-    answer = max([check_farm_of_strings(sorted_list, current_string) for current_string in sorted_list])
-    print(answer)
+    print(check_farm_of_strings(sorted_list) + 1)
